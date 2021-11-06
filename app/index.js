67811bd3f750afc7b9a4abc041552c53776603e7
Krzysztof Jankowski <krzysztof.jankowski@canalplus.pl>
2021-11-06 12:52:11 +0100

    ok

--- app/index.js
+++ app/index.js
@@ -19,48 +19,15 @@ const swaggerOptions = {
     }
   },
   // ['.routes/*.js']
-  apis: ["index.js"]
+  // apis: ["index.js"]
+  apis: ['./app/**/*.js'],
+
 };
 const bc = new Blockchain();
 
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-// Routes
-/**
- * @swagger
- * /customers:
- *  get:
- *    description: Use to request all customers
- *    responses:
- *      '200':
- *        description: A successful response
- */
-app.get("/customers", (req, res) => {
-  res.status(200).send("Customer results");
-});
-
-/**
- * @swagger
- * /customers:
- *    put:
- *      description: Use to return all customers
- *    parameters:
- *      - name: customer
- *        in: query
- *        description: Name of our customer
- *        required: false
- *        schema:
- *          type: string
- *          format: string
- *    responses:
- *      '201':
- *        description: Successfully created user
- */
-app.put("/customer", (req, res) => {
-  res.status(200).send("Successfully updated customer");
-});
 
 
 // Routes
