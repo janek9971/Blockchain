418168a0b3de4b54bc8ae6958cfa61a1dc0f1ae7
Krzysztof Jankowski <krzysztof.jankowski@canalplus.pl>
2021-11-07 15:46:13 +0100

    added transaction update

--- chain-util.js
+++ chain-util.js
@@ -1,4 +1,5 @@
 const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
 const { v4: uuidv4 } = require('uuid');
 const ec = new EC('secp256k1');
 
@@ -7,9 +8,17 @@ class ChainUtil {
         return ec.genKeyPair();
     }
 
-    static id(){
+    static id() {
         return uuidv4();
     }
+
+    static hash(data) {
+        return SHA256(JSON.stringify(data)).toString();
+    }
+
+    static verifySignature(publicKey, signature, messageHash) {
+        return ec.keyFromPublic(publicKey, 'hex').verify(messageHash, signature);
+      }
 }
 
 module.exports = ChainUtil;
--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const ChainUtil = require('../chain-util');
+const SHA256 = require('crypto-js/sha256');
 
 class Transaction {
     constructor() {
@@ -8,16 +9,16 @@ class Transaction {
     }
 
     static newTransaction(senderWallet, recipient, amount) {
-        if(amount > senderWallet.balance){
+        if (amount > senderWallet.balance) {
             console.log(`Amount: ${amount} exceeds balance`)
             return;
         }
         const transaction = new this();
-        
+
         transaction.outputs.push(...[
             {
-              amount: senderWallet.balance - amount,
-              address: senderWallet.publicKey
+                amount: senderWallet.balance - amount,
+                address: senderWallet.publicKey
             },
             {
                 amount,
@@ -25,7 +26,44 @@ class Transaction {
             }
         ])
 
+        Transaction.signTransaction(transaction, senderWallet);
+
         return transaction;
+    }
+
+    update(senderWallet, recipient, amount) {
+        const senderOutput = this.outputs.find(output => output.address = senderWallet.publicKey);
+
+        if (amount > senderOutput.amount) {
+            console.log(`Amount: ${amount} exceeds balance`)
+            return;
+        }
+
+        senderOutput.amount = senderOutput.amount - amount;
+        this.outputs.push({amount, address: recipient});
+
+        Transaction.signTransaction(this, senderWallet);
+
+        return this;
+    }
+
+    static messageHash(transaction) {
+        return ChainUtil.hash(transaction.outputs);
+    }
+
+    static verifyTransaction(transaction) {
+        const { signature, address } = transaction.input
+        const messageHash = Transaction.messageHash(transaction);
+        return ChainUtil.verifySignature(address, signature, messageHash)
+    }
+
+    static signTransaction(transaction, senderWallet) {
+        transaction.input = {
+            timestamp: Date.now(),
+            amount: senderWallet.balance,
+            address: senderWallet.publicKey,
+            signature: senderWallet.sign(Transaction.messageHash(transaction)),
+        }
     }
 }
 
